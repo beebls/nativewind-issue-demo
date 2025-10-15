e882595d8118ef72d7652dfc4354dd04ef5c2876
Beebles <102569435+beebls@users.noreply.github.com>
2025-10-15 16:06:14 -0600

    add in nativewind4

---
+++ nativewind4/App.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import './global.css';
+
+// This is a brand new app created with `npx rn-new nativewind4test --nativewind`
+export default function App() {
+  return (
+    <View className="h-full w-full items-center justify-center bg-red-800">
+      <View className="bg-blue-400" style={{ padding: 100 }}>
+        <View style={{ backgroundColor: 'white', width: 100, height: 100 }}></View>
+      </View>
+    </View>
+  );
+}
