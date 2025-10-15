c2a60d6f3d9aadbd6839703f5e5838cad3459dc5
Beebles <102569435+beebls@users.noreply.github.com>
2025-10-15 16:22:11 -0600

    update demo

--- nativewind4/App.tsx
+++ nativewind4/App.tsx
@@ -5,7 +5,7 @@ import './global.css';
 export default function App() {
   return (
     <View className="h-full w-full items-center justify-center bg-red-800">
-      <View className="bg-blue-400" style={{ padding: 100 }}>
+      <View className="bg-blue-400 p-4" style={{ borderWidth: 10 }}>
         <View style={{ backgroundColor: 'white', width: 100, height: 100 }}></View>
       </View>
     </View>
